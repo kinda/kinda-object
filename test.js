d01999689e493ce436f2a930d905f5398b5d48b4
fishead <zhchuan7@gmail.com>
2014-11-13 18:35:06 +0800

    add test

---
+++ test.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var assert = require('chai').assert;
+var EventManager = require('./event-manager');
+var KindaObject = require('./');
+
+suite('EventManager', function() {
+  var foo = EventManager.instantiate();
+
+  test('beginEventSession', function() {
+
+  });
+});
