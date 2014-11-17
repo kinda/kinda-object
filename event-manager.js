30360bae27adc5edfc06ceb9efe5b0d6655fec20
Manuel Vila <mvila@3base.com>
2014-11-17 10:36:07 +0800

    fixed documentation

--- event-manager.js
+++ event-manager.js
@@ -10,16 +10,10 @@ var globalEventSession = {
 };
 
 var EventManager = KindaClass.extend('EventManager', function() {
-  /**
-   * Increase globalEventSession count when begin event session.
-   */
   this.beginEventSession = function() {
     globalEventSession.count++;
   };
 
-  /**
-   * Function to emit all events session on global and reset the count to 0.
-   */
   this.endEventSession = function() {
     if (!globalEventSession.count)
       throw new Error('cannot end a non opened session');
@@ -27,7 +21,7 @@ var EventManager = KindaClass.extend('EventManager', function() {
       while (globalEventSession.deferredEvents.length) {
         var event = globalEventSession.deferredEvents.shift();
         var args = [event.name].concat(event.args);
-        this.doEmit.apply(event.object, args);
+        this._emit.apply(event.object, args);
       }
       globalEventSession.objects.forEach(function(obj) {
         delete obj._eventSession;
@@ -38,10 +32,6 @@ var EventManager = KindaClass.extend('EventManager', function() {
     globalEventSession.count--;
   };
 
-  /**
-   * Function to deal with event which pass into function.
-   * @param {function} fn - event function need to call.
-   */
   this.runEventSession = function(fn) {
     this.beginEventSession();
     try {
@@ -51,9 +41,6 @@ var EventManager = KindaClass.extend('EventManager', function() {
     }
   };
 
-  /**
-   * Function to get the event sesson on self.
-   */
   this.getEventSession = function() {
     if (!globalEventSession.count)
       throw new Error('no opened session');
@@ -64,18 +51,10 @@ var EventManager = KindaClass.extend('EventManager', function() {
     return this._eventSession;
   };
 
-  /**
-   * check
-   */
   this.hasEventSession = function() {
     return !!globalEventSession.count;
   };
 
-  /**
-   * Function to get event listener on self.
-   * @param {string} name - the name of event listener.
-   * @param {boolean} createIfUndefined - if true, create event listener if it is undefined.
-  */
   this.getEventListeners = function(name, createIfUndefined) {
     if (!this.hasOwnProperty('_eventListeners')) {
       if (!createIfUndefined) return;
@@ -88,23 +67,12 @@ var EventManager = KindaClass.extend('EventManager', function() {
     return this._eventListeners[name];
   }
 
-  /**
-   * add event handl function to specify event listener.
-   * @param {string} name - the name of event listener.
-   * @param {function} fn - the event handle function.
-  */
   this.on = function(name, fn) {
     var listeners = this.getEventListeners(name, true);
     listeners.push(fn);
     return fn;
   };
 
-  /**
-   * remove event handl function from specify event listener.
-   * @param {string} name - the name of event listener.
-   * @param {function} fn - the event handle function to be removed,
-   * if does not specify, remove all event handle functions.
-  */
   this.off = function(name, fn) { // TODO: event removing in proto
     var listeners = this.getEventListeners(name);
     if (!listeners) return;
@@ -116,10 +84,6 @@ var EventManager = KindaClass.extend('EventManager', function() {
     if (index !== -1) listeners.splice(index, 1);
   };
 
-  /**
-   * Function to emit event specified by name.
-   * @param {string} name - the name of event which need to be emit.
-  */
   this.emit = function(name) {
     if (globalEventSession.count) {
       var session = this.getEventSession();
@@ -128,28 +92,18 @@ var EventManager = KindaClass.extend('EventManager', function() {
         return; // Ignore the event if it was already emitted with the same args
       session[name] = args;
     }
-    this.doEmit.apply(this, arguments);
+    this._emit.apply(this, arguments);
   };
 
-  /**
-   * Function to do emit thing
-   * @param {string} name - the name of event listener.
-  */
-  this.doEmit = function(name) {
+  this._emit = function(name) {
     // console.log('[' + this.getClass().name + ']', name, arguments[1], arguments[2]);
     var args = Array.prototype.slice.call(arguments, 1);
     callListners.call(this, name, this, args);
     var proto = Object.getPrototypeOf(this);
-    if (proto.doEmit)
+    if (proto._emit)
       callListners.call(proto, name, this, args);
   };
 
-  /**
-   * Function to call all events in the specify event listener.
-   * @param {string} name - the name of event listener.
-   * @param {object} object reference by this.
-   * @param {array} arguments passed to event listener.
-  */
   var callListners = function(name, thisArg, args) {
     var listeners = this.getEventListeners(name);
     if (!listeners) return;
@@ -174,13 +128,10 @@ var EventManager = KindaClass.extend('EventManager', function() {
         session[name] = args;
       globalEventSession.deferredEvents.push({ object: this, name: name, args: args });
     } else {
-      this.doEmit.apply(this, arguments);
+      this._emit.apply(this, arguments);
     }
   };
 
-  /**
-   * async version of getEventListeners
-  */
   this.getAsyncEventListeners = function(name, createIfUndefined) {
     if (!this.hasOwnProperty('_asyncEventListeners')) {
       if (!createIfUndefined) return;
--- test.js
+++ test.js
@@ -1,13 +1,8 @@
 "use strict";
 
 var assert = require('chai').assert;
-var EventManager = require('./event-manager');
 var KindaObject = require('./');
 
-suite('EventManager', function() {
-  var foo = EventManager.instantiate();
-
-  test('beginEventSession', function() {
-
-  });
+suite('KindaObject', function() {
+  // ...
 });
