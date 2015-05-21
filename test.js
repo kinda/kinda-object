6519dab2cd11b778dca48e0ec27990a97e6132aa
Manuel Vila <mvila@3base.com>
2015-05-21 16:19:55 +0800

    add more tests

--- test.js
+++ test.js
@@ -12,9 +12,37 @@ suite('KindaObject', function() {
       };
     });
 
-    assert.strictEqual(Person.name, 'Person');
-
     let person = Person.create('Jean', 'Dupont');
+    assert.strictEqual(person.firstname, 'Jean');
+    assert.strictEqual(person.lastname, 'Dupont');
+  });
+
+  test('serializer', function() {
+    let Person = KindaObject.extend('Person', function() {
+      this.serializer = function() {
+        return { firstname: this.firstname, lastname: this.lastname };
+      };
+    });
+
+    let person = Person.create();
+    person.firstname = 'Jean';
+    person.lastname = 'Dupont';
+    let json = person.serialize();
+    assert.deepEqual(
+      json,
+      { firstname: 'Jean', lastname: 'Dupont' }
+    );
+  });
+
+  test('unserializer', function() {
+    let Person = KindaObject.extend('Person', function() {
+      this.unserializer = function(json) {
+        this.firstname = json.firstname;
+        this.lastname = json.lastname;
+      };
+    });
+
+    let person = Person.unserialize({ firstname: 'Jean', lastname: 'Dupont' });
     assert.strictEqual(person.firstname, 'Jean');
     assert.strictEqual(person.lastname, 'Dupont');
   });
