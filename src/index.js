2b8706025de1cdab53615dd5a361062fb60c6da8
Manuel Vila <mvila@3base.com>
2015-05-23 09:06:29 +0800

    add unserializeFrom()

--- src/index.js
+++ src/index.js
@@ -16,6 +16,10 @@ let KindaObject = KindaClass.extend('KindaObject', function() {
     return klass.createFrom(this, ...args);
   };
 
+  this.unserialize = function(klass, json) {
+    return klass.unserializeFrom(this, json);
+  };
+
   Object.defineProperty(this, 'serializer', {
     get() {
       return this._serializer;
@@ -40,23 +44,32 @@ let KindaObject = KindaClass.extend('KindaObject', function() {
   });
 });
 
-KindaObject.create = function(...args) {
-  return this.createFrom(undefined, ...args);
-};
-
-KindaObject.createFrom = function(parent, ...args) {
+KindaObject.instantiateFrom = function(parent) {
   let obj = this.instantiate();
   if (parent && parent.context) {
     obj.context = Object.create(parent.context);
   } else {
     obj.context = {};
   }
+  return obj;
+};
+
+KindaObject.create = function(...args) {
+  return this.createFrom(undefined, ...args);
+};
+
+KindaObject.createFrom = function(parent, ...args) {
+  let obj = this.instantiateFrom(parent);
   if (obj.creator) obj.creator(...args);
   return obj;
 };
 
 KindaObject.unserialize = function(json) {
-  let obj = this.instantiate();
+  return this.unserializeFrom(undefined, json);
+};
+
+KindaObject.unserializeFrom = function(parent, json) {
+  let obj = this.instantiateFrom(parent);
   if (!obj.unserializer) throw new Error('unserializer is undefined');
   obj.unserializer(json);
   return obj;
