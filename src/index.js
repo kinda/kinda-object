fd88580349608462501162327fae6497bd4a9464
Manuel Vila <mvila@3base.com>
2015-08-04 13:24:11 +0800

    merge KindaClass and KindaObject

--- src/index.js
+++ src/index.js
@@ -1,40 +1,83 @@
 'use strict';
 
-let KindaClass = require('kinda-class');
+let semver = require('semver');
+let pkg = require('../package.json');
 
-let KindaObject = KindaClass.extend('KindaObject', function() {
-  Object.defineProperty(this, 'creator', {
-    get() {
-      return this._creator;
-    },
-    set(creator) {
-      this._creator = creator;
+let KindaObject = {
+  _name: 'KindaObject',
+  _version: pkg.version,
+  isKindaClass: true
+};
+
+Object.defineProperty(KindaObject, 'name', {
+  get() {
+    return this._name;
+  }
+});
+
+Object.defineProperty(KindaObject, 'version', {
+  get() {
+    return this._version;
+  }
+});
+
+Object.defineProperty(KindaObject, 'prototype', {
+  get() {
+    if (!this._prototype) {
+      this._prototype = this.constructPrototype();
     }
-  });
+    return this._prototype;
+  }
+});
 
-  Object.defineProperty(this, 'serializer', {
-    get() {
-      return this._serializer;
-    },
-    set(serializer) {
-      this._serializer = serializer;
-    }
-  });
+KindaObject.constructor = function() {};
 
-  this.serialize = this.toJSON = function() {
-    if (!this.serializer) throw new Error('serializer is undefined');
-    return this.serializer();
+KindaObject.extend = function(name, version, constructor) {
+  if (typeof name !== 'string') {
+    constructor = version;
+    version = name;
+    name = 'Sub' + this._name;
+  }
+
+  if (typeof version !== 'string') {
+    constructor = version;
+    version = undefined;
+  }
+
+  let parent = this;
+  let child = {
+    _name: name,
+    _version: version
   };
 
-  Object.defineProperty(this, 'unserializer', {
-    get() {
-      return this._unserializer;
-    },
-    set(unserializer) {
-      this._unserializer = unserializer;
+  // Copy class properties
+  let keys = Object.getOwnPropertyNames(parent);
+  for (let key of keys) {
+    if (key.startsWith('_')) continue; // Don't copy property starting with a '_'
+    let descriptor = Object.getOwnPropertyDescriptor(parent, key);
+    Object.defineProperty(child, key, descriptor);
+  }
+
+  child.constructor = function() {
+    this.include(parent);
+    if (!constructor) return;
+    if (typeof constructor === 'function') {
+      constructor.call(this);
+    } else {
+      let constructorKeys = Object.getOwnPropertyNames(constructor);
+      for (let key of constructorKeys) {
+        let descriptor = Object.getOwnPropertyDescriptor(constructor, key);
+        Object.defineProperty(this, key, descriptor);
+      }
     }
-  });
-});
+  };
+
+  return child;
+};
+
+KindaObject.instantiate = function() {
+  return Object.create(this.prototype);
+};
 
 KindaObject.create = function(...args) {
   let obj = this.instantiate();
@@ -47,6 +90,111 @@ KindaObject.unserialize = function(...args) {
   if (!obj.unserializer) throw new Error('unserializer is undefined');
   obj.unserializer(...args);
   return obj;
+};
+
+KindaObject.isClassOf = function(instance) {
+  return !!(instance && instance.isInstanceOf && instance.isInstanceOf(this));
+};
+
+KindaObject.constructPrototype = function() {
+  let currentClass = this;
+  let superclasses = [];
+  let creator;
+  let serializer;
+  let unserializer;
+
+  let checkCompatibility = function(v1, v2) {
+    if (semver.satisfies(v1, '^' + v2)) return true;
+    if (semver.satisfies(v2, '^' + v1)) return true;
+    return false;
+  };
+
+  let compareClasses = function(a, b, errorIfNotCompatible) {
+    if (a.name !== b.name) return false;
+    if (!a.version || !b.version) return true;
+    if (!checkCompatibility(a.version, b.version)) {
+      if (errorIfNotCompatible) {
+        throw new Error(`class ${a.name} v${a.version} is not compatible with class ${b.name} v${b.version}`);
+      }
+      return false;
+    }
+    if (semver.lte(a.version, b.version)) return true;
+    return false;
+  };
+
+  let prototype = {};
+
+  Object.defineProperty(prototype, 'class', {
+    get() {
+      return currentClass;
+    }
+  });
+
+  Object.defineProperty(prototype, 'superclasses', {
+    get() {
+      return superclasses;
+    }
+  });
+
+  Object.defineProperty(prototype, 'prototype', {
+    get() {
+      return prototype;
+    }
+  });
+
+  Object.defineProperty(prototype, 'creator', {
+    get() {
+      return creator;
+    },
+    set(val) {
+      creator = val;
+    }
+  });
+
+  Object.defineProperty(prototype, 'serializer', {
+    get() {
+      return serializer;
+    },
+    set(val) {
+      serializer = val;
+    }
+  });
+
+  Object.defineProperty(prototype, 'unserializer', {
+    get() {
+      return unserializer;
+    },
+    set(val) {
+      unserializer = val;
+    }
+  });
+
+  prototype.include = function(other) {
+    let isAlreadyIncluded = this.superclasses.some(superclass => {
+      return compareClasses(other, superclass, true);
+    });
+    if (isAlreadyIncluded) return this;
+    other.constructor.call(this);
+    this.superclasses.push(other);
+    return this;
+  };
+
+  prototype.isInstanceOf = function(other) {
+    if (!(other && other.isKindaClass)) return false;
+    if (compareClasses(this.class, other)) return true;
+    return this.superclasses.some(superclass => {
+      return compareClasses(superclass, other);
+    });
+  };
+
+  prototype.serialize = prototype.toJSON = function() {
+    if (!this.serializer) throw new Error('serializer is undefined');
+    return this.serializer();
+  };
+
+  this.constructor.call(prototype);
+
+  return prototype;
 };
 
 module.exports = KindaObject;
--- test.js
+++ test.js
@@ -4,46 +4,269 @@ let assert = require('chai').assert;
 let KindaObject = require('./src');
 
 suite('KindaObject', function() {
-  test('creator', function() {
-    let Person = KindaObject.extend('Person', function() {
-      this.creator = function(firstname, lastname) {
-        this.firstname = firstname;
-        this.lastname = lastname;
+  suite('Simple classes hierarchy', function() {
+    let Foo = KindaObject.extend('Foo', function() {
+      this.cool = 'very';
+    });
+
+    Foo.hello = 'Hello';
+    Foo._bye = 'Bye';
+
+    let Bar = Foo.extend('Bar', function() {
+      this.isCold = function() {
+        return this.cool === 'very' ? 'yes' : 'no';
       };
     });
 
-    let person = Person.create('Jean', 'Dupont');
-    assert.strictEqual(person.firstname, 'Jean');
-    assert.strictEqual(person.lastname, 'Dupont');
+    let Baz = KindaObject.extend('Baz', function() {
+      this.include(Bar);
+    });
+
+    let UnnamedClass = Foo.extend();
+
+    suite('Class methods', function() {
+      test('extend()', function() {
+        assert.strictEqual(Bar.hello, 'Hello');
+        assert.isUndefined(Bar._bye);
+      });
+
+      test('name', function() {
+        assert.strictEqual(Foo.name, 'Foo');
+        assert.strictEqual(UnnamedClass.name, 'SubFoo');
+      });
+
+      test('instantiate()', function() {
+        let bar = Bar.instantiate();
+        assert.strictEqual(bar.cool, 'very');
+        assert.isFunction(bar.isCold);
+      });
+
+      test('create()', function() {
+        let Person = KindaObject.extend('Person', function() {
+          this.creator = function(firstname, lastname) {
+            this.firstname = firstname;
+            this.lastname = lastname;
+          };
+        });
+
+        let person = Person.create('Jean', 'Dupont');
+        assert.strictEqual(person.firstname, 'Jean');
+        assert.strictEqual(person.lastname, 'Dupont');
+      });
+
+      test('prototype', function() {
+        let bar = Bar.instantiate();
+        assert.strictEqual(Bar.prototype.isCold, bar.isCold);
+      });
+
+      test('isKindaClass', function() {
+        assert.isTrue(Bar.isKindaClass);
+      });
+
+      test('isClassOf()', function() {
+        let baz = Baz.instantiate();
+        assert.isTrue(Baz.isClassOf(baz));
+        assert.isTrue(Bar.isClassOf(baz));
+        assert.isTrue(Foo.isClassOf(baz));
+        assert.isTrue(KindaObject.isClassOf(baz));
+        let Qux = KindaObject.extend('Qux');
+        assert.isFalse(Qux.isClassOf(baz));
+      });
+
+      test('unserialize()', function() {
+        let Person = KindaObject.extend('Person', function() {
+          this.unserializer = function(json) {
+            this.firstname = json.firstname;
+            this.lastname = json.lastname;
+          };
+        });
+
+        let person = Person.unserialize({ firstname: 'Jean', lastname: 'Dupont' });
+        assert.strictEqual(person.firstname, 'Jean');
+        assert.strictEqual(person.lastname, 'Dupont');
+      });
+    });
+
+    suite('Instance methods', function() {
+      test('include()', function() {
+        let baz = Baz.instantiate();
+        assert.strictEqual(baz.cool, 'very');
+        assert.isFunction(baz.isCold);
+      });
+
+      test('class', function() {
+        let foo = Foo.instantiate();
+        assert.strictEqual(foo.class, Foo);
+      });
+
+      test('superclasses', function() {
+        let foo = Foo.instantiate();
+        assert.deepEqual(foo.superclasses, [KindaObject]);
+        let bar = Bar.instantiate();
+        assert.deepEqual(bar.superclasses, [KindaObject, Foo]);
+        let baz = Baz.instantiate();
+        assert.deepEqual(baz.superclasses, [KindaObject, Foo, Bar]);
+      });
+
+      test('prototype', function() {
+        let bar = Bar.instantiate();
+        assert.strictEqual(Bar.prototype.isCold, bar.isCold);
+      });
+
+      test('isInstanceOf()', function() {
+        let foo = Foo.instantiate();
+        assert.isTrue(foo.isInstanceOf(Foo));
+        assert.isFalse(foo.isInstanceOf(Bar));
+      });
+
+      test('serialize()', function() {
+        let Person = KindaObject.extend('Person', function() {
+          this.serializer = function() {
+            return { firstname: this.firstname, lastname: this.lastname };
+          };
+        });
+
+        let person = Person.create();
+        person.firstname = 'Jean';
+        person.lastname = 'Dupont';
+        let json = person.serialize();
+        assert.deepEqual(
+          json,
+          { firstname: 'Jean', lastname: 'Dupont' }
+        );
+      });
+    });
   });
 
-  test('serializer', function() {
-    let Person = KindaObject.extend('Person', function() {
-      this.serializer = function() {
-        return { firstname: this.firstname, lastname: this.lastname };
-      };
-    });
+  suite('Object constructor', function() {
+    test('simple class', function() {
+      let French = KindaObject.extend('French', {
+        hello: 'Bonjour',
+        bye: 'Au revoir'
+      });
 
-    let person = Person.create();
-    person.firstname = 'Jean';
-    person.lastname = 'Dupont';
-    let json = person.serialize();
-    assert.deepEqual(
-      json,
-      { firstname: 'Jean', lastname: 'Dupont' }
-    );
+      assert.strictEqual(French.prototype.hello, 'Bonjour');
+      assert.strictEqual(French.prototype.bye, 'Au revoir');
+    });
   });
 
-  test('unserializer', function() {
-    let Person = KindaObject.extend('Person', function() {
-      this.unserializer = function(json) {
-        this.firstname = json.firstname;
-        this.lastname = json.lastname;
-      };
+  suite('Name conflict', function() {
+    test('extend a class with the same name', function() {
+      let Person = KindaObject.extend('Person', function() {
+        this.isNice = 'yes';
+        this.isCool = 'yes';
+      });
+
+      Person = Person.extend('Person', function() {
+        this.isCool = 'absolutely';
+      });
+
+      let person = Person.instantiate();
+      assert.strictEqual(person.isNice, 'yes');
+      assert.strictEqual(person.isCool, 'absolutely');
+
+      Person = Person.extend('Person', function() {
+        this.isCool = 'definitely';
+      });
+
+      person = Person.instantiate();
+      assert.strictEqual(person.isNice, 'yes');
+      assert.strictEqual(person.isCool, 'definitely');
+    });
+  });
+
+  suite('Diamond problem', function() {
+    let count = 0;
+
+    let Top = KindaObject.extend('Top', function() {
+      count++;
     });
 
-    let person = Person.unserialize({ firstname: 'Jean', lastname: 'Dupont' });
-    assert.strictEqual(person.firstname, 'Jean');
-    assert.strictEqual(person.lastname, 'Dupont');
+    let Left = Top.extend('Left');
+
+    let Right = Top.extend('Right');
+
+    let Bottom = Top.extend('Bottom', function() {
+      this.include(Left);
+      this.include(Right);
+    });
+
+    test('top constructor should only be called once', function() {
+      assert.strictEqual(count, 0);
+      Bottom.instantiate();
+      assert.strictEqual(count, 1);
+      Bottom.instantiate();
+      assert.strictEqual(count, 1);
+    });
+  });
+
+  suite('Versioning', function() {
+    test('version', function() {
+      let A = KindaObject.extend('A');
+      assert.isUndefined(A.version);
+      let B = KindaObject.extend('B', '0.1.0');
+      assert.strictEqual(B.version, '0.1.0');
+    });
+
+    test('isInstanceOf()', function() {
+      let A010 = KindaObject.extend('A', '0.1.0');
+      assert.isTrue(A010.prototype.isInstanceOf(A010));
+
+      let A015 = KindaObject.extend('A', '0.1.5');
+      assert.isTrue(A010.prototype.isInstanceOf(A015));
+      assert.isFalse(A015.prototype.isInstanceOf(A010));
+
+      let B015 = KindaObject.extend('B', '0.1.5');
+      assert.isFalse(A010.prototype.isInstanceOf(B015));
+
+      let A024 = KindaObject.extend('A', '0.2.4');
+      assert.isFalse(A015.prototype.isInstanceOf(A024));
+      assert.isFalse(A024.prototype.isInstanceOf(A015));
+
+      let A117 = KindaObject.extend('A', '1.1.7');
+      let A141 = KindaObject.extend('A', '1.4.1');
+      assert.isTrue(A117.prototype.isInstanceOf(A141));
+      assert.isFalse(A141.prototype.isInstanceOf(A117));
+
+      let A202 = KindaObject.extend('A', '2.0.2');
+      assert.isFalse(A117.prototype.isInstanceOf(A202));
+      assert.isFalse(A202.prototype.isInstanceOf(A117));
+    });
+
+    test('include()', function() {
+      let a010Constructed, a015Constructed;
+      let A010 = KindaObject.extend('A', '0.1.0', function() {
+        a010Constructed = true;
+      });
+      let A015 = KindaObject.extend('A', '0.1.5', function() {
+        a015Constructed = true;
+      });
+      let A020 = KindaObject.extend('A', '0.2.0');
+
+      a010Constructed = false;
+      a015Constructed = false;
+      (KindaObject.extend(function() {
+        this.include(A010);
+        this.include(A015);
+      })).instantiate();
+      assert.isTrue(a010Constructed);
+      assert.isTrue(a015Constructed);
+
+      a010Constructed = false;
+      a015Constructed = false;
+      (KindaObject.extend(function() {
+        this.include(A015);
+        this.include(A010);
+      })).instantiate();
+      assert.isTrue(a015Constructed);
+      assert.isFalse(a010Constructed);
+
+      assert.throws(function() {
+        (KindaObject.extend(function() {
+          this.include(A015);
+          this.include(A020);
+        })).instantiate();
+      });
+    });
   });
 });
